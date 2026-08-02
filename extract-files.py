1861e87248e3c0f99d4d740ec66a3c32980ded93
Alex <simutalexandru@yahoo.com>
2026-08-02 13:12:13 +0300

    sm8150-common: Fix rescontrol HAL startup
        F libc: Fatal signal 11 (SIGSEGV), fault addr 0x571
        W HidlServiceManagement: Waited one second for vendor.lge.hardware.powerhint.rescontrol@2.0::IResControl/default
    
      Preserve x24 and x25 around the broken per-app XML constructor
    
    Change-Id: Ie8e8ac13038935f94a6b1418cd76e005bf007fe1

--- extract-files.py
+++ extract-files.py
@@ -49,6 +49,19 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/vendor.sensors.sscrpcd.rc': blob_fixup()
         .regex_replace('class early_hal', 'class core'),
+    'vendor/lib64/vendor.lge.hardware.powerhint.rescontrol@2.0-common.so': blob_fixup()
+        # Preserve x24/x25 across the broken per-app XML constructor. Its
+        # missing-file path violates the arm64 ABI and corrupts linker state.
+        .sig_replace(
+            'FD 7B BE A9 F3 0B 00 F9 FD 03 00 91 53 00 00 B0 '
+            '73 96 42 F9 41 01 80 52 E0 03 13 AA 3C 20 00 94 '
+            'E1 03 13 AA F3 0B 40 F9 E0 FF FF F0 42 00 00 B0 '
+            '00 20 2E 91 42 00 00 91 FD 7B C2 A8 38 20 00 14',
+            'FD 7B BD A9 F3 63 01 A9 F9 13 00 F9 53 00 00 B0 '
+            '73 96 42 F9 41 01 80 52 E0 03 13 AA 3C 20 00 94 '
+            'E1 03 13 AA F3 63 41 A9 F9 13 40 F9 40 D1 FF 10 '
+            'E2 74 04 10 FD 7B 40 A9 FF C3 00 91 38 20 00 14',
+        ),
     ('vendor/lib/libwvhidl.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
